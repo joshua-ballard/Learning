050cb00db9fbb422ad6d5829d635b3d77f640d86
Joshua Ballard <ballard@jlab.org>
2016-05-10 16:39:57 -0400

    minor formatting

--- LS_to_CCon/LS_to_CCon.py
+++ LS_to_CCon/LS_to_CCon.py
@@ -2,35 +2,30 @@
 
 import argparse
 import csv
-import sys
 
-def main(readfile):
-  writefile = readfile[:-4] + '.crv'
-  # build the header (fixed by CryoCon for Cernox sensors)
-  headerlines = [['ACR'], ['-1.0'], ['ohms']]
+# deal with input arguments
+parser = argparse.ArgumentParser(description='Convert LS .dat to Cryocon .crv')
+parser.add_argument('-i', '--input', help='Input file name', required=True)
+args = parser.parse_args()
 
-  # file reads & writes
-  with open(readfile, 'r') as infilehandle, open(writefile, 'w') as outfilehandle:
-    curvereader = csv.reader(infilehandle, delimiter=' ', skipinitialspace=True)
-    curvewriter = csv.writer(outfilehandle, delimiter='\t', lineterminator='\n')
+readfile = args.input
+writefile = args.input[:-4] + '.crv'
 
-    # set up and write the header
-    curvewriter.writerow([readfile[:-4]])
-    for line in headerlines:
-      curvewriter.writerow(line)
+# build the header (fixed by CryoCon for Cernox sensors)
+headerlines = [['ACR'], ['-1.0'], ['ohms']]
 
-    for rownum, row in enumerate(curvereader):
-      if rownum > 2:
-        curvewriter.writerow([row[1], row[0]])
-    # standard .crv file ends with semicolon
-    curvewriter.writerow([';'])
+# file reads & writes
+with open(readfile, 'r') as infilehandle, open(writefile, 'w') as outfilehandle:
+  curvereader = csv.reader(infilehandle, delimiter=' ', skipinitialspace=True)
+  curvewriter = csv.writer(outfilehandle, delimiter='\t', lineterminator='\n')
 
+  # set up and write the header
+  curvewriter.writerow([readfile[:-4]])
+  for line in headerlines:
+    curvewriter.writerow(line)
 
-if __name__ == '__main__':
-  # deal with input arguments
-  parser = argparse.ArgumentParser(description='Convert LS .dat to Cryocon .crv')
-  parser.add_argument('-i', '--input', help='Input file name', required=True)
-  args = parser.parse_args()
-
-  readfile = args.input
-  sys.exit(main(readfile))
+  for rownum, row in enumerate(curvereader):
+    if rownum > 2:
+      curvewriter.writerow([row[1], row[0]])
+  # standard .crv file ends with semicolon
+  curvewriter.writerow([';'])
