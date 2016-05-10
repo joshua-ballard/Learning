e6c536317f99a12d07c1a7b593ec384c157244a0
Joshua Ballard <ballard@jlab.org>
2016-05-10 13:10:30 -0400

    separated argument parsing from main

--- LS_to_CCon/LS_to_CCon.py
+++ LS_to_CCon/LS_to_CCon.py
@@ -2,30 +2,35 @@
 
 import argparse
 import csv
+import sys
 
-# deal with input arguments
-parser = argparse.ArgumentParser(description='Convert LS .dat to Cryocon .crv')
-parser.add_argument('-i', '--input', help='Input file name', required=True)
-args = parser.parse_args()
+def main(readfile):
+  writefile = readfile[:-4] + '.crv'
+  # build the header (fixed by CryoCon for Cernox sensors)
+  headerlines = [['ACR'], ['-1.0'], ['ohms']]
 
-readfile = args.input
-writefile = args.input[:-4] + '.crv'
+  # file reads & writes
+  with open(readfile, 'r') as infilehandle, open(writefile, 'w') as outfilehandle:
+    curvereader = csv.reader(infilehandle, delimiter=' ', skipinitialspace=True)
+    curvewriter = csv.writer(outfilehandle, delimiter='\t', lineterminator='\n')
 
-# build the header (fixed by CryoCon for Cernox sensors)
-headerlines = [['ACR'], ['-1.0'], ['ohms']]
+    # set up and write the header
+    curvewriter.writerow([readfile[:-4]])
+    for line in headerlines:
+      curvewriter.writerow(line)
 
-# file reads & writes
-with open(readfile, 'r') as infilehandle, open(writefile, 'w') as outfilehandle:
-  curvereader = csv.reader(infilehandle, delimiter=' ', skipinitialspace=True)
-  curvewriter = csv.writer(outfilehandle, delimiter='\t', lineterminator='\n')
+    for rownum, row in enumerate(curvereader):
+      if rownum > 2:
+        curvewriter.writerow([row[1], row[0]])
+    # standard .crv file ends with semicolon
+    curvewriter.writerow([';'])
 
-  # set up and write the header
-  curvewriter.writerow([readfile[:-4]])
-  for line in headerlines:
-    curvewriter.writerow(line)
 
-  for rownum, row in enumerate(curvereader):
-    if rownum > 2:
-      curvewriter.writerow([row[1], row[0]])
-  # standard .crv file ends with semicolon
-  curvewriter.writerow([';'])
+if __name__ == '__main__':
+  # deal with input arguments
+  parser = argparse.ArgumentParser(description='Convert LS .dat to Cryocon .crv')
+  parser.add_argument('-i', '--input', help='Input file name', required=True)
+  args = parser.parse_args()
+
+  readfile = args.input
+  sys.exit(main(readfile))
